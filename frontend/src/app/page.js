d5a750d1708ea92a04b9ba521cba7cf61210231d
LucasHzl <hezil.lucas@gmail.com>
2024-06-07 15:04:49 +0200

    add decode tk

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -20,7 +20,9 @@ export default function Home() {
     return JSON.parse(text)
   }
   
-
+  useEffect(() => {
+    DecodeJwtTokenPayload("eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE3MTc2MDQ4MzIsImV4cCI6MTcxNzYwODQzMiwicm9sZXMiOlsiUk9MRV9VU0VSIl0sInVzZXJuYW1lIjoibHVjYXNAZ21haWwuY29tIn0.RrxBsknR25isLNhqnb5ZJ0qVyE9HoCpIlYT4BEsnUFkIgmJIGBA6KQFqdTnyYWozj-KSmLlzTLlpTLb9dOLX2tEPwBP3tNVROXFX62LbV0WsfJp8vWB3rSSqS101ZiNTlL-C8QavTi0VvM_Ay83MbbN67odRqjUBI6XwpeFOTJl7JonHahenAWZHttpvzElu6uSI6BYoPhQnU_pbxrgoQZ_kfE5yfQya7FBlfA4QXZtrG_gqAZsgWE8nfXOFYy3EzQMEYvoJXpT6GssfpLq1BvrlwPHwlmpCiCJ73Lc2uwuQHsMoKGb5_dT9gsQntUYIPjWIJnq_22_ZBy5UEjarVg")
+}, [])
 
   return (
     <main>
